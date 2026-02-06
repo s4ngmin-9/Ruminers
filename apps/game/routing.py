f7f72fe53b117d0cc696d98046d13f118b62a859
s4ngmin-9 <s4ngmin9@naver.comm>
2026-02-06 16:09:33 +0900

    프로젝트 구조 리팩토링 및 앱 구조 정리

---
+++ apps/game/routing.py
@@ -0,0 +1,6 @@
+from django.urls import re_path
+from .consumers import GameConsumer
+
+websocket_urlpatterns = [
+    re_path(r"ws/test/(?P<room_id>\w+)/$", GameConsumer.as_asgi()),
+]
--- game/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from .consumers import GameConsumer
-
-websocket_urlpatterns = [
-    re_path(r"^ws/game/(?P<room_id>\w+)/$", GameConsumer.as_asgi()),
-    re_path(r"^ws/test/(?P<room_id>\w+)/$", GameConsumer.as_asgi()),
-]
